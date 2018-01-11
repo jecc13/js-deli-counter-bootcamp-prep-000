b76bbace89959066c9e1b527d4284ce4345fbe0d
Jennifer E <je@jenniferevenson.com>
2018-01-11 09:06:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, newCustomer) {
+  katzDeliLine.push(newCustomer);
+  return katzDeliLine.length;
+}
+
+function nowServing(katzDeliLine) {
+  
+}
