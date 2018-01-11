e19910c030e964c7021b3e01b234059d84cf5553
Jennifer E <je@jenniferevenson.com>
2018-01-11 09:28:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, newCustomer) {
-  var greeting = `Welcome, ${newCustomer}. You are number ${katzDeliLine.length`
   katzDeliLine.push(newCustomer);
-  
-  return katzDeliLine.length;
+  var greeting = `Welcome, ${newCustomer}. You are number ${katzDeliLine.length} in line.`;
+  return greeting;
 }
 
 function nowServing(katzDeliLine) {
   var nextUp = "There is nobody waiting to be served!";
   if (katzDeliLine.length > 0) {
-    nextUp = katzDeliLine[0];
+    nextUp = `Currently serving ${katzDeliLine[0]}.`;
     katzDeliLine.shift();
   }
   return NextUp;
@@ -21,7 +20,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     lineString = "The line is currently: "
     for (var i = 0; i < katzDeliLine.length; i++) {
-      lineString = lineString + (i + 1) + ". " + katzDeliLine[i];
+      lineString = lineString + (i + 1) + `. ${katzDeliLine[i]}`;
       if (i <= (katzDeliLine.length - 2) ) {
         lineString = lineString + ", ";
       }
