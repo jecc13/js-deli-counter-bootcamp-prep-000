7427ab6d49ff3a4785f8e4f934b46eef88a3606b
Jennifer E <je@jenniferevenson.com>
2018-01-11 09:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,24 @@ function takeANumber(katzDeliLine, newCustomer) {
 }
 
 function nowServing(katzDeliLine) {
-  var nextUp = 
+  var nextUp = "There is nobody waiting to be served!";
+  if (katzDeliLine.length > 0) {
+    nextUp = katzDeliLine[0];
+    katzDeliLine.shift();
+  }
+  return NextUp;
+}
+
+function currentLine(katzDeliLine) {
+  var lineString = "The line is currently empty."
+  if (katzDeliLine.length > 0) {
+    lineString = "The line is currently: "
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      lineString = lineString + (i + 1) + ". " + katzDeliLine[i];
+      if (i <= (katzDeliLine.length - 2) ) {
+        lineString = lineString + ", ";
+      }
+    }
+  }
+  return lineString;
 }
